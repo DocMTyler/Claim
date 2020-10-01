de007e804c497422a0ab821a190a2da2b391f49f
DocMTyler <tylerhougland@att.net>
2020-10-01 16:47:21 -0400

    Initial commit

---
+++ Claim/ClaimRepositoryQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimRepository
+{
+    public class ClaimRepositoryQueue
+    {
+        Queue<Claim> claimItem = new Queue<Claim>();
+        
+        //Create a claim, add to queue 
+        public bool AddClaimToQueue(Claim claim)
+        {
+            int beforeEnqueue = claimItem.Count;
+            claimItem.Enqueue(claim);
+            bool isAddedToQ = (claimItem.Count > beforeEnqueue);
+            return isAddedToQ;
+        }
+
+        //Read all claims
+        public Queue<Claim> ReadClaims()
+        {
+            return claimItem;
+        }
+
+        //Delete a claim, remove from queue
+        public bool RemoveClaimFromQueue(Claim claim)
+        {
+            int beforeDequeue = claimItem.Count;
+            claimItem.Dequeue();
+            bool isRemovedFromQ = (claimItem.Count < beforeDequeue);
+            return isRemovedFromQ;
+        }
+
+    }
+}
---
+++ ClaimRepository_Tests/ClaimRepositoryTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using ClaimRepository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClaimRepository_Tests
+{
+    [TestClass]
+    public class ClaimRepositoryTests
+    {
+        [TestMethod]
+        public void AddClaimToQueueTest()
+        {
+            //Arrange
+            Claim claim = new Claim();
+            ClaimRepositoryQueue claimRepositoryQueue = new ClaimRepositoryQueue();
+
+            //ACT
+            bool addedToQueue = claimRepositoryQueue.AddClaimToQueue(claim);
+
+            //Assert
+            Assert.IsTrue(addedToQueue);
+            Console.WriteLine(addedToQueue);
+        }
+
+        [TestMethod]
+        public void ReadClaimsTest()
+        {
+            //Arrange
+            Claim claim = new Claim();
+            ClaimRepositoryQueue claimRepositoryQueue = new ClaimRepositoryQueue();
+            claimRepositoryQueue.AddClaimToQueue(claim);
+
+            //ACT
+            var claimQueue = claimRepositoryQueue.ReadClaims();
+
+            //Assert
+            bool queueHasClaims = claimQueue.Contains(claim);
+            Assert.IsTrue(queueHasClaims);
+            Console.WriteLine(queueHasClaims);
+        }
+
+        [TestMethod]
+        public void RemoveClaimFromQueueTest()
+        {
+            //Arrange
+            Claim claim = new Claim();
+            ClaimRepositoryQueue claimRepositoryQueue = new ClaimRepositoryQueue();
+
+            claimRepositoryQueue.AddClaimToQueue(claim);
+
+            //ACT
+            bool wasClaimDequeued = claimRepositoryQueue.RemoveClaimFromQueue(claim);
+
+            //Assert
+            Assert.IsTrue(wasClaimDequeued);
+            Console.WriteLine(wasClaimDequeued);
+        }
+    }
+}
